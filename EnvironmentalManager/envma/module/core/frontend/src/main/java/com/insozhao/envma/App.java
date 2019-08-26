93d7a8148f9d38f3fc29e02ac6efdc4a060d4911
insozhao <insozhao@163.com>
2019-08-26 10:42:07 +0800

    项目基本目录

---
+++ EnvironmentalManager/envma/module/core/frontend/src/main/java/com/insozhao/envma/App.java
@@ -0,0 +1,13 @@
+package com.insozhao.envma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
